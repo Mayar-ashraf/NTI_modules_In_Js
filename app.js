905f573a0de19755fcbfe4f5673c799e058e850a
Mayar-ashraf <mayarashraf2002@gmail.com>
2025-07-20 20:06:18 +0300

    created utils.js file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import * as math from "./math.js"
 import greeting from "./greet.js"
+import capitalize from "./utils.js";
 
 var sum = math.add(2, 3);
 console.log(`The sum of 2 and 3 is ${sum}`);
@@ -10,3 +11,6 @@ console.log(`The difference between 2 and 3 is ${diff}`);
 console.log(`PI = ${math.pi}`);
 
 console.log(greeting("Mayar"));
+
+console.log(`Capitalization the first of "mayar" is ${capitalize("mayar")}`);
+
